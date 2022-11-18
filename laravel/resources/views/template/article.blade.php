9d5d3a18fa697fb19041c4cf70fa55223c1fe812
Ferdiansyah <ferdiansyah@kompas.com>
2022-11-18 14:33:19 +0700

    remove laravel app

--- laravel/resources/views/template/article.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-
-@section('content')
-<h1>ini text dengan layout 2</h1>
-@endsection
--- laravel/resources/views/template/index.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-
-@section('content')
-<h1>ini template index</h1>
-@endsection
