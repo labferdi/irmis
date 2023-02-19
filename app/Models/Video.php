708f621089353400fe43e34e0a3739125d57044f
Ferdiansyah <ferdiansyah@kompas.com>
2023-02-19 14:59:00 +0700

    video model & migrations

---
+++ app/Models/Video.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+// php artisan make:model Video --migration
+
+
+class Video extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_02_19_074619_create_videos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');              // varchar 255
+            $table->string('tags', 200);         // varchar 200
+            $table->string('url_link', 100);     // varchar 100
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
